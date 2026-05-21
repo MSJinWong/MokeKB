11e154934656d721073dfa7418f1592b92ef92ed
Jin <jswangjin@foxmail.com>
2026-05-21 23:53:11 +0800

    feat(ui): add LucideIcon wrapper component

---
+++ ui/src/components/lucide-icon/LucideIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <Icon :icon="`lucide:${name}`" :width="size" :height="size" />
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+withDefaults(
+  defineProps<{
+    name: string
+    size?: number | string
+  }>(),
+  {
+    size: 18,
+  },
+)
+</script>
